e2e4bc156fe9c458df501545310d3035c2be06ff
xms_7104 <xms881126@gmail.com>
2025-12-05 00:45:45 +0800

    github page deploy setting.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/2025-12-01-resume-layout">
       <App />
     </BrowserRouter>
   </StrictMode>

e1008588e1f721d78600a178d151e909083e380b
xms_7104 <xms881126@gmail.com>
2025-12-05 01:48:08 +0800

    fixed typo

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,21 +1,21 @@
 function Information(){
     const userInformation = [
-        {title: '手機', contant: '0958119288'}, {title: 'Email', contant: 'xms881126@gmail.com'}
+        {title: '手機', content: '0958119288'}, {title: 'Email', content: 'xms881126@gmail.com'}
     ]
 
     function userInformationLayout(){
         return userInformation.map((item, index) => {
             let href = null;
             if (item.title === '手機') {
-                href = `tel:${item.contant}`;
+                href = `tel:${item.content}`;
             } else if (item.title.toLowerCase() === 'email') {
-                href = `mailto:${item.contant}`;
+                href = `mailto:${item.content}`;
             }
 
             return(
                 <div className="information_text" key={index}>
                     <p>{item.title}：</p>
-                    <p className="information_href">{href ? (<a href={href}>{item.contant}</a>) : (item.contant)}</p>
+                    <p className="information_href">{href ? (<a href={href}>{item.content}</a>) : (item.content)}</p>
                 </div>
             )
         })
